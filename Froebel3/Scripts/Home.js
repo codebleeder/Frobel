2d3f6e09d68afb6b7ad54867fdfd9a5fec61ece4
codebleeder <sharad.shivmath@gmail.com>
2017-09-02 15:48:05 -0400

    Made triangle equilateral, added right triangle, added code to generate semi circle

--- Froebel3/Scripts/Home.js
+++ Froebel3/Scripts/Home.js
@@ -20,19 +20,44 @@ function canvas_side() {
             fill: 'rgb(239, 157, 249)'
         },
 
-        triangle: {
-            top: 100,
+        square: {
+            top: 80,
             left: 50,
-            width: 70,
+            width: 40,
             height: 40,
+            fill: 'rgb(239, 157, 249)'
+        },
+
+        triangle: {
+            top: 140,
+            left: 50,
+            width: 40,
+            height: 34.6410161514,
             fill: '#fe5d26',            
         },
 
+        right_triangle: {
+            top: 210,
+            left: 60,
+            width: 56.5685424949,
+            height: 28.284271245,
+            fill: '#fe5d26',
+            angle: 135
+        },
+
         circle: {
-            top: 170,
+            top: 240,
             left: 50,
             radius: 25,
             fill: '#f2c078'
+        },
+
+        semi_circle: {
+            top: 240,
+            left: 50,
+            radius: 25,
+            fill: '#f2c078',
+            endAngle: Math.PI
         }
     };
 
@@ -47,40 +72,88 @@ function canvas_side() {
             hasControls: false
         },
 
-        triangle: {
-            top: 100,
+        square: {
+            top: 80,
             left: 50,
-            width: 70,
+            width: 40,
             height: 40,
+            fill: 'rgb(239, 157, 249)',
+            hasBorders: false,
+            hasControls: false 
+        },
+
+        triangle: {
+            top: 140,
+            left: 50,
+            width: 40,
+            height: 34.6410161514,
             fill: '#fe5d26',
             hasBorders: false,
             hasControls: false
         },
 
+        right_triangle: {
+            top: 210,
+            left: 60,
+            width: 56.5685424949,
+            height: 28.284271245,
+            fill: '#fe5d26',
+            hasBorders: false,
+            hasControls: false,
+            angle: 135
+        },
+
         circle: {
-            top: 170,
+            top: 240,
             left: 50,
             radius: 25,
             fill: '#f2c078',
             hasBorders: false,
             hasControls: false
-        }
+        },
+
+        semi_circle: {
+            top: 300,
+            left: 50,
+            radius: 25,
+            fill: '#f2c078',
+            startAngle: 0,
+            endAngle: Math.PI,
+            hasBorders: false,
+            hasControls: false
+        },
     };
     
-    function add_shape(shape) {
-        if (shape === 'rect') {
+    function add_shape(shape, top) {
+        if (shape === 'rect' && top === shapes_side.rect.top) {
             var rect_main = new fabric.Rect(shapes_main.rect);
             canvas_main.add(rect_main);
         }
 
-        if (shape == 'triangle') {
+        if (shape === 'rect' && top === shapes_side.square.top) {
+            var square_main = new fabric.Rect(shapes_main.square);
+            canvas_main.add(square_main);
+        }
+
+        if (shape == 'triangle' && top==shapes_side.triangle.top) {
             var triangle_main = new fabric.Triangle(shapes_main.triangle);
             canvas_main.add(triangle_main);
         }
 
-        if (shape == 'circle') {
+        if (shape == 'triangle' && top == shapes_side.right_triangle.top) {
+            var right_triangle_main = new fabric.Triangle(shapes_main.right_triangle);
+            canvas_main.add(right_triangle_main);
+        }
+
+
+        if (shape == 'circle' && top === shapes_side.circle.top) {
             var circle_main = new fabric.Circle(shapes_main.circle);
             canvas_main.add(circle_main);
+        }
+
+        if (shape == 'circle' && top === shapes_side.semi_circle.top) {
+            var semi_circle_main = new fabric.Circle(shapes_main.semi_circle);
+            canvas_main.add(semi_circle_main);
         }
     }
     
@@ -106,7 +179,7 @@ function canvas_side() {
 
     canvas_side.on('object:selected', function (opt) {        
         var shape_type = opt.target.get('type');
-        add_shape(shape_type);
+        add_shape(shape_type, opt.target.top, opt.target.left);
     });
 
     var rect_side = new fabric.Rect(shapes_side.rect);
@@ -115,6 +188,12 @@ function canvas_side() {
     canvas_side.add(triangle_side);
     var circle_side = new fabric.Circle(shapes_side.circle);
     canvas_side.add(circle_side);
+    var square_side = new fabric.Rect(shapes_side.square);
+    canvas_side.add(square_side);
+    var right_triangle_side = new fabric.Triangle(shapes_side.right_triangle);
+    canvas_side.add(right_triangle_side);
+    var semi_circle_side = new fabric.Circle(shapes_side.semi_circle);
+    canvas_side.add(semi_circle_side);
 }
 
 $(document).ready(function () {
