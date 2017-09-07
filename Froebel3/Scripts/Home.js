9e90513404e625c165894c7e8d523b924edc7531
codebleeder <sharad.shivmath@gmail.com>
2017-09-07 13:13:13 -0400

    load facebook sdk

--- Froebel3/Scripts/Home.js
+++ Froebel3/Scripts/Home.js
@@ -1,5 +1,24 @@
 ﻿// EaselJS code
 function init() {
+    // load facebook SDK
+    window.fbAsyncInit = function () {
+        FB.init({
+            appId: 'your-app-id',
+            autoLogAppEvents: true,
+            xfbml: true,
+            version: 'v2.10'
+        });
+        FB.AppEvents.logPageView();
+    };
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) { return; }
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
     canvas_main();
     canvas_side();
     function setTextColor(picker) {
